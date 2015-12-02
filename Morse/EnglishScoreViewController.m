c0db3569841347ccd43cc28f6214a02ac9ca231d
TomasArnar <Tomas@Tomass-MacBook-Pro.local>
2015-12-01 22:27:19 -0600

    ss

--- Morse/EnglishScoreViewController.m
+++ Morse/EnglishScoreViewController.m
@@ -12,12 +12,18 @@
 @interface EnglishScoreViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *englishTextField;
 @property (weak, nonatomic) IBOutlet UITextField *morseTextField;
+@property (weak, nonatomic) IBOutlet UILabel *scoreField;
+@property int score;
 @end
 
 @implementation EnglishScoreViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.score = 0;
     
     self.englishTextField.text = [RandomWordList getRandomWords];
     self.morseTextField.text = @"";
@@ -29,6 +35,81 @@
     //[self dismissViewControllerAnimated:YES completion:nil];
 }
 
+
+- (IBAction)submit:(UIButton *)sender {
+    
+    NSDictionary *englishCharacterArray = @{
+                                            
+                                            @"/" : @" ",
+                                            @".-" : @"A",
+                                            @"-..." : @"B",
+                                            @"-.-."  : @"C",
+                                            @"-.."  : @"D",
+                                            @"."  : @"E",
+                                            @"..-."  : @"F",
+                                            @"--."  : @"G",
+                                            @"...." :  @"H",
+                                            @".." :  @"I",
+                                            @".---"  : @"J",
+                                            @"-.-"  : @"K",
+                                            @".-.."  : @"L",
+                                            @"--." : @"M",
+                                            @"-." : @"N" ,
+                                            @"---" : @"O",
+                                            @".--." : @"P",
+                                            @"--.-" : @"Q",
+                                            @".-." : @"R",
+                                            @"..." : @"S",
+                                            @"-" : @"T",
+                                            @"..-" : @"U",
+                                            @"...-" : @"V",
+                                            @".--" : @"W",
+                                            @"-..-":@"X",
+                                            @"-.--":@"Y",
+                                            @"--..":@"Z",
+                                            @".----":@"1",
+                                            @"..---":@"2",
+                                            @"...--":@"3",
+                                            @"....-":@"4",
+                                            @".....":@"5",
+                                            @"-....":@"6",
+                                            @"--...":@"7",
+                                            @"---..":@"8",
+                                            @"----.": @"9",
+                                            @"-----":@"0",
+                                            @" ":@" ",
+                                            @"--..--":@",",
+                                            @".-.-.-":@".",
+                                            @"..--..":@"?",
+                                            @".----.":@"\'",
+                                            @"-.-.--":@"!",
+                                            @"-..-.":@"/",
+                                            @"-.--.":@"(",
+                                            @"-.--.-":@")",
+                                            @".-...":@"&",
+                                            @"---...":@":",
+                                            @"-.-.-.":@";",
+                                            @"-...-":@"=",
+                                            @".-.-.":@"+",
+                                            @"-....-":@"-",
+                                            @"..--.-":@"_",
+                                            @".-..-.":@"\"",
+                                            @"...-..-":@"$",
+                                            @".--.-.":@"@",
+                                            };
+
+    
+    if ([self.morseTextField.text isEqualToString:[englishCharacterArray objectForKeyedSubscript:self.englishTextField.text]]) {
+        self.score = self.score + 1;
+    }
+    else {
+        self.score = self.score - 1;
+    }
+    self.scoreField.text = [NSString stringWithFormat:@"%i",self.score];
+    
+    self.englishTextField.text = [RandomWordList getRandomWords];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -37,5 +118,8 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+-(BOOL)prefersStatusBarHidden{
+    return YES;
+}
 
 @end
